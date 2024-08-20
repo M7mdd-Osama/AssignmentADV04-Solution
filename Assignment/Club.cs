d4f60e2d715b8a0fa6467bdcefe6ea3823021329
Mohamed Osama Ali <ana20332183@gmail.com>
2024-08-20 18:22:57 +0300

    Done

---
+++ Assignment/Club.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Club
+    {
+        public int ClubID { get; set; }
+        public string ClubName { get; set; }
+
+        public List<Employee> Members = new List<Employee>();
+        public void AddMember(Employee E)
+        {
+            Members.Add(E);
+            E.EmployeeLayOff += RemoveMember;
+        }
+        public void RemoveMember(Employee employee, LayOffCause cause)
+        {
+            if (cause == LayOffCause.VacationStockLessThanZero)
+            {
+                Members.Remove(employee);
+            }
+        }
+    }
+}
---
+++ Assignment/SalesPerson.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class SalesPerson : Employee
+    {
+        public int AchievedTarget { get; set; }
+        public bool CheckTarget(int Quota)
+        {
+            if (AchievedTarget < Quota)
+            {
+                OnEmployeeLayOff(LayOffCause.SalesTargetNotMet);
+                return false;
+            }
+            return true;
+        }
+        protected override void OnEmployeeLayOff(LayOffCause cause)
+        {
+            if (cause == LayOffCause.VacationStockLessThanZero)
+            {
+                return;
+            }
+            base.OnEmployeeLayOff(cause);
+        }
+    }
+}
